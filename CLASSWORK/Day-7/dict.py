0be5663c109e99d57b01756a7b4c94b6fc9e66e9
24aiml091-sketch <24aiml091@ipec.org.in>
2026-07-06 11:06:08 +0530

    dictinary

---
+++ CLASSWORK/Day-7/dict.py
@@ -0,0 +1,15 @@
+''' create a dictionary of student name and their id
+ '''
+#-------------coding---------------------
+#create empty dictionary
+student_id = {}
+
+#add the elements
+student_id[ 1 ]= 'sanjay'
+student_id[ 2 ]= 'Aadi'
+student_id[ 3 ]= 'Adarsh'
+
+
+print(student_id)
+#---------------
+#----------output-----------
