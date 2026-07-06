076edb2df7a09210c4e9e27ca3149d6e4596d0c8
24aiml091-sketch <24aiml091@ipec.org.in>
2026-07-06 10:56:44 +0530

    Update CLASSWORK and LABWORK

---
+++ CLASSWORK/Day-4/multiplication_table_generator.py
@@ -0,0 +1,18 @@
+'''Problem Statement
+ Write a Python program that accepts a number from the user and displays its multiplication table up to 20. '''
+'''Sample Output 
+ Enter Number: 8 
+  8 x 1 = 8
+   8 x 2 = 16 ... 
+   8 x 20 = 160 '''
+#------------------------------------------
+#------coding-----------------------------
+number = int(input("Enter Number: "))
+#validation
+if(number < 0):
+    exit("Enter the positive number")
+#multiplication table
+counter = 1
+
+    
+    
